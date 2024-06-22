7dc8eaaa90db6308cfd10615b111ad9e37e14de5
Shirsendu <palshirsendu03@gmail.com>
2024-06-22 21:36:43 +0530

    Fixed postgre with sql server

--- app_files/__init__.py
+++ app_files/__init__.py
@@ -4,8 +4,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+internal_var = "postgresql://users_db_n95m_user:k1utNV6zjMigFYGYoIWlifWinDZ66G3K@dpg-cprcsljv2p9s73a4kl8g-a/users_db_n95m"
+external_var = "postgresql://users_db_n95m_user:k1utNV6zjMigFYGYoIWlifWinDZ66G3K@dpg-cprcsljv2p9s73a4kl8g-a.oregon-postgres.render.com/users_db_n95m"
 app = flask.Flask(__name__)
-os.environ["DATABASE_URL"] = "postgresql://users_db_n95m_user:k1utNV6zjMigFYGYoIWlifWinDZ66G3K@dpg-cprcsljv2p9s73a4kl8g-a.oregon-postgres.render.com/users_db_n95m"
+
+os.environ["DATABASE_URL"] = internal_var
 app.config['SECRET_KEY'] = "super_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 # postgresql://users_db_n95m_user:k1utNV6zjMigFYGYoIWlifWinDZ66G3K@dpg-cprcsljv2p9s73a4kl8g-a.oregon-postgres.render.com/users_db_n95m
